eb848eb1de032b400a7c3385b2f29547d0807af8
Ashwani-Sharma <sharma.ashwani007@gmail.com>
2020-07-06 17:18:41 +0530

    dynamic time updated

--- Exercise2_Digital-and-analog-clock/js/ex2_digital-analog-clock.js
+++ Exercise2_Digital-and-analog-clock/js/ex2_digital-analog-clock.js
@@ -1,70 +1,87 @@
 class Clock {
-  constructor(hours, minutes, seconds, meridiem) {
-    let date = new Date();
+  constructor(button) {
+    this.button = document.querySelectorAll('[data-time]');
+  }
 
-    this.hours = date.getHours();
-    this.minutes = date.getMinutes();
-    this.seconds = date.getSeconds();
-    this.meridiem = 'AM';
+  setTime() {
+    var date = new Date();
+
+    return {
+      hours: date.getHours(),
+      minutes: date.getMinutes(),
+      seconds: date.getSeconds(),
+      meridiem: 'AM'
+    }
   }
 
   setHourAndMeridiem() {
-    let hrs = this.hours;
+    let hrs = this.setTime().hours;
+    let mrdm = this.setTime().meridiem;
 
     if(hrs == 0) {
       hrs = 12;
     }
     else if(hrs > 12) {
-      this.meridiem = 'PM';
+      mrdm = 'PM';
       hrs = hrs - 12;
     }
 
-    return [hrs, this.meridiem];
+    return [hrs, mrdm];
   }
 }
 
 class AnalogClock extends Clock {
-  constructor(minutes, seconds) {
-    super(minutes, seconds);
-  }
-
   showAnalogTime() {
     let hour = this.setHourAndMeridiem()[0];
     let hourHand = hour * 30;
-    let minuteHand = this.minutes * 6;
-    let secondHand = this.seconds * 6;
+    let minuteHand = this.setTime().minutes * 6;
+    let secondHand = this.setTime().seconds * 6;
     let showTime = ('Hour hand at ' + hourHand + 'deg \n' + 'Minute hand at ' + minuteHand + 'deg \n' + 'Second hand at ' + secondHand + 'deg');
 
     alert(showTime);
   }
+
+  init() {
+    let _this = this;
+    let btn = this.button;
+
+    btn.forEach(function (element) {
+      element.addEventListener('click', function() {
+        if(this.dataset.time == 'analog') {
+          _this.showAnalogTime();
+        }
+      });
+    });
+  }
 }
 
 class DigitalClock extends Clock {
-  constructor(minutes, seconds) {
-    super(minutes, seconds);
-  }
-
   showDigitalTime() {
     let hour = this.setHourAndMeridiem()[0];
     let mrdns = this.setHourAndMeridiem()[1];
-    let showTime = `${hour} : ${this.minutes} : ${this.seconds} ${mrdns}`;
+    let showTime = `${hour} : ${this.setTime().minutes} : ${this.setTime().seconds} ${mrdns}`;
 
     alert(showTime);
+  }
+
+  init() {
+    let _this = this;
+    let btn = this.button;
+
+    btn.forEach(function (element) {
+      element.addEventListener('click', function() {
+        if(this.dataset.time == 'digital') {
+          _this.showDigitalTime();
+        }
+      });
+    });
   }
 }
 
 window.onload = function () {
-  let btnAnalog = document.getElementById('analog');
-  let btnDigital = document.getElementById('digital');
-
   let analogClock = new AnalogClock();
   let digitalClock = new DigitalClock();
 
-  btnAnalog.addEventListener('click', function() {
-    analogClock.showAnalogTime();
-  });
-
-  btnDigital.addEventListener('click', function() {
-    digitalClock.showDigitalTime();
-  });
+  analogClock.init();
+  digitalClock.init();
 }
