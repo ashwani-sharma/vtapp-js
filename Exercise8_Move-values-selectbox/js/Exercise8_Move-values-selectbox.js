87591e3494ebd6136bdefe4d8a661690d7782015
Ashwani-Sharma <sharma.ashwani007@gmail.com>
2020-07-27 13:47:48 +0530

    code updated

--- Exercise8_Move-values-selectbox/js/Exercise8_Move-values-selectbox.js
+++ Exercise8_Move-values-selectbox/js/Exercise8_Move-values-selectbox.js
@@ -1,8 +1,11 @@
 /* jshint esversion: 6 */
 
 class MoveValues {
-  constructor(DOM) {
-    this.elements = DOM;
+  constructor(options) {
+    this.elements = options;
+    this.btns = this.elements.buttons;
+    this.selectSource = this.elements.selectPrimary;
+    this.selectDestination = this.elements.selectSecondary;
   }
 
   moveSelectedOptions(src, dest) {
@@ -16,28 +19,28 @@ class MoveValues {
   bindButtonEvents(btn) {
     btn.addEventListener('click', () => {
       if(btn.dataset.button == 'add') {
-        this.moveSelectedOptions(this.elements.selectPrimary, this.elements.selectSecondary);
+        this.moveSelectedOptions(this.selectSource, this.selectDestination);
       }
       else if(btn.dataset.button == 'remove') {
-        this.moveSelectedOptions(this.elements.selectSecondary, this.elements.selectPrimary);
+        this.moveSelectedOptions(this.selectDestination, this.selectSource);
       }
     });
   }
 
   init() {
-    this.elements.buttons.forEach((elem) => {
+    this.btns.forEach((elem) => {
       this.bindButtonEvents(elem);
     });
   }
 }
 
 window.onload = function() {
-  let DOM_Elements = {
+  let options = {
     buttons: document.querySelectorAll("[data-button]"),
     selectPrimary: document.querySelector("[data-select='primary']"),
     selectSecondary: document.querySelector("[data-select='secondary']")
   };
 
-  let changeValues = new MoveValues(DOM_Elements);
+  let changeValues = new MoveValues(options);
   changeValues.init();
 };
