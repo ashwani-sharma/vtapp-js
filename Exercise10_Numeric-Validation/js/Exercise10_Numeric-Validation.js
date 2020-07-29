aa8ebfd5b686073e22f0b241bba4925a6d3b119d
Ashwani-Sharma <sharma.ashwani007@gmail.com>
2020-07-29 15:19:27 +0530

    exercise 10 added

---
+++ Exercise10_Numeric-Validation/js/Exercise10_Numeric-Validation.js
@@ -0,0 +1,44 @@
+const regex = /^[\-\+]?[0-9]{1,}(\.[0-9]{0,})?\d$/;
+
+class NumericValidation {
+  constructor(options) {
+    this.elements = options;
+    this.form = this.elements.form;
+    this.numberField = this.elements.number;
+    this.result = this.elements.result;
+  }
+
+  checkInputValidation(pattern, input, message) {
+    if(!pattern.test(input.value)) {
+      alert('invalid input');
+      message.value = 'false';
+      input.focus();
+      return false;
+    }
+    else {
+      message.value = 'true';
+      return true;
+    }
+  }
+
+  formSubmission() {
+    this.form.addEventListener('submit', (e) => {
+      if(this.checkInputValidation(regex, this.numberField, this.result)) {
+        return true;
+      }
+
+      e.preventDefault();
+    });
+  }
+}
+
+window.onload = function() {
+  const options = {
+    form: document.querySelector('[data-id=form]'),
+    number: document.querySelector('[data-input=number]'),
+    result: document.querySelector('[data-input=result]')
+  };
+
+  let validateForm = new NumericValidation(options);
+  validateForm.formSubmission();
+}
